2ad9b7a759c702e2738d007597a09252160e53c0
Vipul Bagde <vipulbagde1897@gmail.com>
2025-03-20 20:24:12 +0530

    Done simple OCR

--- main.py
+++ main.py
@@ -7,4 +7,15 @@ images ='images/word.jpg'
 
 reader = easyocr.Reader(['en'], gpu=False)
 result = reader.readtext(images)
-print(result)
+#print(result[0][0][0])
+
+top_left= tuple(result[0][0][0])
+bottom_right= tuple(result[0][0][2])
+text=result[0][1]
+font= cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+
+img=cv2.imread(images)
+img=cv2.rectangle(img, top_left,bottom_right,(0,255,0),5)
+img=cv2.putText(img,text,top_left,font,.5,(255,255,255),2,cv2.LINE_AA)
+plt.imshow(img)
+plt.show()
